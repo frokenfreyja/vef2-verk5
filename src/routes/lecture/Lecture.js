868dcaee4f6eb0be59c81df2da787897375a31d6
frokenfreyja <freyjasigur@gmail.com>
2019-03-22 19:13:46 +0000

    .

--- src/routes/lecture/Lecture.js
+++ src/routes/lecture/Lecture.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-
 import { getLecture, toggleLectureFinish, loadSavedLectures } from '../../api';
+import { Route } from 'react-router-dom';
 
-import Header from '../../components/header/Header';
 import Item from '../../components/item/Item';
+import Header from '../../components/header/Header';
 import NotFound from '../../routes/not-found/NotFound';
 
-import '../../components/lecture/Lecture.scss';
-import '../../components/item/Item.scss';
-
 export default class Lecture extends Component {
-  
+
   state = {
-      lecture: getLecture(this.props.match.params.slug),
-      check: '\u2713',
-    }
-  
-  handleClick = slug => (e) => {
+    lecture: getLecture(this.props.match.params.slug),
+  }
+
+  markFinished = (slug) => (e) => {
     const { target } = e;
     const isFinished = target.classList.contains('lecture__finish--finished');
     if (isFinished) {
@@ -27,42 +23,44 @@ export default class Lecture extends Component {
 
     target.classList.toggle('lecture__finish--finished');
     toggleLectureFinish(slug, !isFinished);
-    this.setState({ lecture: getLecture(slug) });
+    this.setState({ lecture : getLecture(this.props.match.params.slug)});
   }
+
   checkFinished = (slug) => {
     const saved = loadSavedLectures();
     return saved.indexOf(slug) >= 0;
   }
 
   render() {
-    const { lecture, check } = this.state;
+    const { lecture } = this.state;
+
+    if(!lecture){
+      return <Route component={NotFound} />
+    }
+
+    const slug = this.props.match.params.slug;
+    const { content } = lecture;
+    const { finished } = lecture;
 
     return (
       <React.Fragment>
-        <Header category={lecture.category} title={lecture.title} image={lecture.image}/>
-        <section className="lecture">
-          <div className="lecture__content">
-            <div className="lecture__row">
-              <div className="lecture__col">
-                {
-                  lecture ? lecture.content.map((element, i) => {
-                    return (<Item key={i} content={element}/>)
-                  }) : <NotFound/>
-                }
-              </div>
-            </div>
-          </div>
-        </section>
+        <Header category={lecture.category} title={lecture.title} image={lecture.image} />
+        
+        <div className="lecture__col">
+          { content.map((item, i) => 
+            <Item key={i} item={item} /> 
+          ) }  
+        </div>
         <footer className="lecture__footer">
-          <button onClick={this.handleClick(lecture.slug)} 
-            className={lecture.finished ? "lecture__finish lecture__finish--finished" : "lecture__finish"}>
-            {
-              (lecture.finished) ? `${check} Fyrirlestur kláraður` : "Klára fyrirlestur"
-            }
-          </button>
+          {finished? 
+            <button onClick={this.markFinished(slug)} className="lecture__finish lecture__finish--finished">✓ Klára fyrirlestur</button>
+            :
+            <button onClick={this.markFinished(slug)} className="lecture__finish">Klára fyrirlestur</button>
+          }
+          
           <a className="lecture__back" href="/">Til baka</a>
         </footer>
       </React.Fragment>
-    );
+    )
   }
 }
